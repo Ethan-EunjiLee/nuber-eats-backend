e5bcf9f0e4cda73fe3ed6cbff66376384ebd7ce4
Ethan-EunjiLEe <ethan.ej.lee@gmail.com>
2021-06-17 23:30:57 +0900

    6/17 9-1

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -2,11 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
   // * beforeAll: 모든 test전에 module을 load
+  // * 앱을 생성하고 다 import하고, typeorm을 이용하고 등등...
   beforeAll(async () => {
     // * UserModule만 테스트하더라도 AppModule을 통으로 import 해야 한다.
     const module: TestingModule = await Test.createTestingModule({
@@ -17,10 +19,16 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-  it.todo('me');
-  it.todo('userProfile');
+  // * 모든 테스트가 끝날 때 마다 application이 종료 & database drop
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    await app.close();
+  });
+
   it.todo('createAccount');
+  it.todo('userProfile');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
